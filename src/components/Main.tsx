1279b20d44ebcaecdbeac0b964b4d14ff5fc01e8
gomdopi <io.gomdopi@gmail.com>
2025-02-10 11:01:07 -0800

    refactor form submission using action

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,36 +1,24 @@
-import { FormEvent, useState } from "react"
+import { useState } from "react"
 
 export default function Main() {
   // the fact that "useState" returns an array allows us to name
   // the returned initial state and setState function whatever we want
   // if it returned an object we would be forced to use whatever
   // name the property of the returned object has instead
-  const [ingredients, setIngredients] = useState([
-    "chicken",
-    "oregano",
-    "cilantro",
-  ])
+  const [ingredients, setIngredients] = useState<string[]>([])
 
-  const ingredientsListItems = ingredients.map((ingredient) => (
+  const ingredientsListItems = ingredients.map(ingredient => (
     <li key={ingredient}>{ingredient}</li>
   ))
 
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault()
-    const formData = new FormData(e.currentTarget as HTMLFormElement)
-    const newIngredient = formData.get("ingredient")
-
-    // It is good to develop the habit of using a callback function
-    // to update state if you ever rely on the old state to determine new state
-    setIngredients((prevIngredients) => [
-      ...prevIngredients,
-      newIngredient as string,
-    ])
+  function addIngredient(formData: FormData) {
+    const newIngredient = formData.get("ingredient") as string
+    setIngredients(prevIngredients => [...prevIngredients, newIngredient])
   }
 
   return (
     <main>
-      <form onSubmit={handleSubmit} className="add-ingredient-form">
+      <form action={addIngredient} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
