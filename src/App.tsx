a9654573d8f951b507bcd7b7b672a69f61caa534
gomdopi <io.gomdopi@gmail.com>
2025-02-09 20:44:16 -0800

    add header component w/ styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Header from "./components/Header"
+
 export default function App() {
-  return <h1>I am APP!</h1>
+  return <Header />
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="chef-claude-icon.png" />
+      <h1>Chef Claude</h1>
+    </header>
+  )
+}
