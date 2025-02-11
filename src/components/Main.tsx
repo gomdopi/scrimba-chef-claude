6dd119424c7cac96c48391c6abae1f244ea1328f
gomdopi <io.gomdopi@gmail.com>
2025-02-10 22:18:41 -0800

    extract into separate components w/o styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import ClaudeRecipe from "./ClaudeRecipe"
+import IngredientsList from "./IngredientsList"
 
 export default function Main() {
   // the fact that "useState" returns an array allows us to name
@@ -6,14 +8,15 @@ export default function Main() {
   // if it returned an object we would be forced to use whatever
   // name the property of the returned object has instead
   const [ingredients, setIngredients] = useState<string[]>([])
-
-  const ingredientsListItems = ingredients.map(ingredient => (
-    <li key={ingredient}>{ingredient}</li>
-  ))
+  const [recipeShown, setRecipeShown] = useState(false)
 
   function addIngredient(formData: FormData) {
     const newIngredient = formData.get("ingredient") as string
     setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+  }
+
+  function toggleRecipeShown() {
+    setRecipeShown(prevRecipeShown => !prevRecipeShown)
   }
 
   return (
@@ -27,7 +30,13 @@ export default function Main() {
         />
         <button type="submit">Add ingredient</button>
       </form>
-      <ul>{ingredientsListItems}</ul>
+      {ingredients.length > 0 && (
+        <IngredientsList
+          ingredients={ingredients}
+          toggleRecipeShown={toggleRecipeShown}
+        />
+      )}
+      {recipeShown && <ClaudeRecipe />}
     </main>
   )
 }
